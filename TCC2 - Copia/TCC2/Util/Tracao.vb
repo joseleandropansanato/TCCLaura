a36bed4f0254b25742868df477412f3f92bdb49b
DESKTOP-PN0NE1G\Usuario <lauramariacuryclivatti@outlook.com>
2022-05-10 21:34:01 -0300

    alteração tração

--- TCC2 - Copia/TCC2/Util/Tracao.vb
+++ TCC2 - Copia/TCC2/Util/Tracao.vb
@@ -6,55 +6,55 @@
 	Public Shared eixoXPecaComposta As Double = 0
 	Public Shared eixoYPecaComposta As Double = 0
 
-    Public Function TensaoTracao(normalTracao As Double, baseX As Double, baseY As Double, c As Double, diametro As Double, d As Double, bw As Double, bf1 As Double, altura As Double, propriedadesGeometricas As PropriedadesGeometricas, tipoSecao As Madeira.TipoSecao, l As Double, b1 As Double, a As Double, elementoFixacaoSelecionado As String, lvinculado As Double, momentoFletorX As Double, momentoFletorY As Double, h1 As Double) As Tracao
+    Public Function Tensao(normalTracao As Double, baseX As Double, baseY As Double, c As Double, diametro As Double, d As Double, bw As Double, bf1 As Double, altura As Double, propriedadesGeometricas As PropriedadesGeometricas, tipoSecao As Madeira.TipoSecao, l As Double, b1 As Double, a As Double, elementoFixacaoSelecionado As String, lvinculado As Double, momentoFletorX As Double, momentoFletorY As Double, h1 As Double) As Tracao
         Dim tracao = New Tracao()
 
         Select Case tipoSecao
             Case Madeira.TipoSecao.Retangular
-                tracao.tensaoTracao = ((normalTracao) / (propriedadesGeometricas.Area)) 'OK
-                Tracao.esbeltezTracaoX = (c * 100 / (Math.Sqrt(propriedadesGeometricas.EixoXmi / propriedadesGeometricas.Area))) 'OK
-                Tracao.esbeltezTracaoY = (c * 100 / (Math.Sqrt(propriedadesGeometricas.EixoYmi / propriedadesGeometricas.Area))) 'OK
+                Tracao.tensaoTracao = ((normalTracao) / (PropriedadesGeometricas.area)) 'OK
+                Tracao.esbeltezTracaoX = (c * 100 / (Math.Sqrt(PropriedadesGeometricas.eixoXmi / PropriedadesGeometricas.area))) 'OK
+                Tracao.esbeltezTracaoY = (c * 100 / (Math.Sqrt(PropriedadesGeometricas.eixoYmi / PropriedadesGeometricas.area))) 'OK
 
             Case Madeira.TipoSecao.Circular
-                tracao.tensaoTracao = normalTracao / propriedadesGeometricas.Area
-                Tracao.esbeltezTracaoX = (diametro / (Math.Sqrt(propriedadesGeometricas.EixoXmi / propriedadesGeometricas.Area)))
-                Tracao.esbeltezTracaoY = (altura / (Math.Sqrt(propriedadesGeometricas.EixoYmi / propriedadesGeometricas.Area)))
+                Tracao.tensaoTracao = normalTracao / PropriedadesGeometricas.area
+                Tracao.esbeltezTracaoX = (diametro / (Math.Sqrt(PropriedadesGeometricas.eixoXmi / PropriedadesGeometricas.area)))
+                Tracao.esbeltezTracaoY = (altura / (Math.Sqrt(PropriedadesGeometricas.eixoYmi / PropriedadesGeometricas.area)))
 
             Case Madeira.TipoSecao.SecaoT
-                tracao.tensaoTracao = normalTracao / propriedadesGeometricas.Area
-                Tracao.esbeltezTracaoX = (bw / (Math.Sqrt(propriedadesGeometricas.EixoXmi / propriedadesGeometricas.Area)))
-                Tracao.esbeltezTracaoY = (d / (Math.Sqrt(propriedadesGeometricas.EixoYmi / propriedadesGeometricas.Area)))
+                Tracao.tensaoTracao = normalTracao / PropriedadesGeometricas.area
+                Tracao.esbeltezTracaoX = (bw / (Math.Sqrt(PropriedadesGeometricas.eixoXmi / PropriedadesGeometricas.area)))
+                Tracao.esbeltezTracaoY = (d / (Math.Sqrt(PropriedadesGeometricas.eixoYmi / PropriedadesGeometricas.area)))
 
             Case Madeira.TipoSecao.SecaoI
-                tracao.tensaoTracao = normalTracao / propriedadesGeometricas.Area
-                Tracao.esbeltezTracaoX = (bf1 / (Math.Sqrt(propriedadesGeometricas.EixoXmi / propriedadesGeometricas.Area)))
-                Tracao.esbeltezTracaoY = (d / (Math.Sqrt(propriedadesGeometricas.EixoYmi / propriedadesGeometricas.Area)))
+                Tracao.tensaoTracao = normalTracao / PropriedadesGeometricas.area
+                Tracao.esbeltezTracaoX = (bf1 / (Math.Sqrt(PropriedadesGeometricas.eixoXmi / PropriedadesGeometricas.area)))
+                Tracao.esbeltezTracaoY = (d / (Math.Sqrt(PropriedadesGeometricas.eixoYmi / PropriedadesGeometricas.area)))
 
             Case Madeira.TipoSecao.Caixao
-                tracao.tensaoTracao = normalTracao / propriedadesGeometricas.Area
-                Tracao.esbeltezTracaoX = (bf1 / (Math.Sqrt(propriedadesGeometricas.EixoXmi / propriedadesGeometricas.Area)))
-                Tracao.esbeltezTracaoY = (d / (Math.Sqrt(propriedadesGeometricas.EixoYmi / propriedadesGeometricas.Area)))
+                Tracao.tensaoTracao = normalTracao / PropriedadesGeometricas.area
+                Tracao.esbeltezTracaoX = (bf1 / (Math.Sqrt(PropriedadesGeometricas.eixoXmi / PropriedadesGeometricas.area)))
+                Tracao.esbeltezTracaoY = (d / (Math.Sqrt(PropriedadesGeometricas.eixoYmi / PropriedadesGeometricas.area)))
 
             Case Madeira.TipoSecao.ElementosJustaposto2
                 'botar o espaçador aqui tb
                 If ElementoJustaposto(l, b1, a, elementoFixacaoSelecionado) Then
                     'tracao.TensaoTracao = normalTracao / propriedadesGeometricas.Area
-                    Tracao.esbeltezTracaoX = ((lvinculado) / (Math.Sqrt(propriedadesGeometricas.EixoXmi / propriedadesGeometricas.Area)))
-                    Tracao.esbeltezTracaoY = ((lvinculado) / (Math.Sqrt(propriedadesGeometricas.EixoYmi / propriedadesGeometricas.Area)))
+                    Tracao.esbeltezTracaoX = ((lvinculado) / (Math.Sqrt(PropriedadesGeometricas.eixoXmi / PropriedadesGeometricas.area)))
+                    Tracao.esbeltezTracaoY = ((lvinculado) / (Math.Sqrt(PropriedadesGeometricas.eixoYmi / PropriedadesGeometricas.area)))
                     'eixo X::
-                    Tracao.eixoXPecaComposta = (normalTracao / propriedadesGeometricas.Area) + (momentoFletorX / propriedadesGeometricas.EixoXmi) * (h1 / 2)
+                    Tracao.eixoXPecaComposta = (normalTracao / PropriedadesGeometricas.area) + (momentoFletorX / PropriedadesGeometricas.eixoXmi) * (h1 / 2)
                     'eixo y::
-                    Tracao.eixoYPecaComposta = (normalTracao / propriedadesGeometricas.Area) + ((momentoFletorY * propriedadesGeometricas.EixoYmi1) / propriedadesGeometricas.EixoYie * propriedadesGeometricas.EixoYmr) + (momentoFletorY / (2 * propriedadesGeometricas.Area * propriedadesGeometricas.AreaA1)) * (1 - 2 * (propriedadesGeometricas.EixoYmi1 / propriedadesGeometricas.EixoYie))
+                    Tracao.eixoYPecaComposta = (normalTracao / PropriedadesGeometricas.area) + ((momentoFletorY * PropriedadesGeometricas.eixoYmi1) / PropriedadesGeometricas.eixoYie * PropriedadesGeometricas.eixoYmr) + (momentoFletorY / (2 * PropriedadesGeometricas.area * PropriedadesGeometricas.areaA1)) * (1 - 2 * (PropriedadesGeometricas.eixoYmi1 / PropriedadesGeometricas.eixoYie))
                 End If
             Case Madeira.TipoSecao.ElementosJustaposto3
                 If ElementoJustaposto(l, b1, a, elementoFixacaoSelecionado) Then
                     'tracao.TensaoTracao = normalTracao / propriedadesGeometricas.Area
-                    Tracao.esbeltezTracaoX = (lvinculado) / propriedadesGeometricas.EixoXrg
-                    Tracao.esbeltezTracaoY = (lvinculado) / (Math.Sqrt(propriedadesGeometricas.EixoYie / propriedadesGeometricas.Area))
+                    Tracao.esbeltezTracaoX = (lvinculado) / PropriedadesGeometricas.eixoXrg
+                    Tracao.esbeltezTracaoY = (lvinculado) / (Math.Sqrt(PropriedadesGeometricas.eixoYie / PropriedadesGeometricas.area))
                     'eixo X::
-                    Tracao.eixoXPecaComposta = (normalTracao / propriedadesGeometricas.Area) + (momentoFletorX / propriedadesGeometricas.EixoXmi) * (h1 / 2)
+                    Tracao.eixoXPecaComposta = (normalTracao / PropriedadesGeometricas.area) + (momentoFletorX / PropriedadesGeometricas.eixoXmi) * (h1 / 2)
                     'eixo y::
-                    Tracao.eixoYPecaComposta = (normalTracao / propriedadesGeometricas.Area) + ((momentoFletorY * propriedadesGeometricas.EixoYmi1) / propriedadesGeometricas.EixoYie * propriedadesGeometricas.EixoYmr) + (momentoFletorY / (3 * propriedadesGeometricas.Area * propriedadesGeometricas.AreaA1)) * (1 - 2 * (propriedadesGeometricas.EixoYmi1 / propriedadesGeometricas.EixoYie))
+                    Tracao.eixoYPecaComposta = (normalTracao / PropriedadesGeometricas.area) + ((momentoFletorY * PropriedadesGeometricas.eixoYmi1) / PropriedadesGeometricas.eixoYie * PropriedadesGeometricas.eixoYmr) + (momentoFletorY / (3 * PropriedadesGeometricas.area * PropriedadesGeometricas.areaA1)) * (1 - 2 * (PropriedadesGeometricas.eixoYmi1 / PropriedadesGeometricas.eixoYie))
                 End If
         End Select
 
